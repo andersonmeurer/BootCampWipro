f22a41f90c315c8c5568e8bc68634139b6c16526
João Melo <jgrbm66@gmail.com>
2022-04-11 22:21:00 -0300

    Criando métodos do account manager

--- src/main/java/br/com/gama/wipro/console/AccountManager.java
+++ src/main/java/br/com/gama/wipro/console/AccountManager.java
@@ -1,5 +1,43 @@
 package br.com.gama.wipro.console;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.gama.wipro.entity.Account;
+
 public class AccountManager {
+
+	private List<Account> Accounts = new ArrayList<Account>();
+
+	public void createAccount(Account account) {
+
+		Accounts.add(account);
+		System.out.println(account);
+
+	}
+
+	public void removeAccount(Integer number) {
+		int index = 0;
+		for (Account account : Accounts) {
+			if (account.getNumber() == number) {
+				index = Accounts.indexOf(account);
+			}
+		}
+		
+		Accounts.remove(index);
+
+	}
+
+	public String getAccount(Integer number) {
+		for (Account account : Accounts) {
+			if (account.getNumber() == number) {
+				System.out.println(account.toString());
+				return account.toString();
+			}
+		}
+		
+		System.out.println("Conta não encontrada");
+		return "";
+	}
 
 }

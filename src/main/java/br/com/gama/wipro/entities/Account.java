96706050ecb82afce6d30c69e2c8fd260422782e
DevThome <thomedvlp@gmail.com>
2022-04-26 23:17:29 -0300

    SpecialAccount unitTests

--- src/main/java/br/com/gama/wipro/entities/Account.java
+++ src/main/java/br/com/gama/wipro/entities/Account.java
@@ -42,9 +42,9 @@ public abstract class Account {
 		this.active = active;
 	}
 
-	public Integer getId() {
-		return this.id;
-	}
+//	public Integer getId() {
+//		return this.id;
+//	}
 
 	public void setId(Integer id) {
 		this.id = id;
